cc804a7d57ba8e13f4ae7f9db82ff8fa6ad43c82
Vaibhav Agarwal <141799049+AgarwalVaibhav-20@users.noreply.github.com>
2025-07-28 18:29:18 +0530

    Update supabase-client.ts

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -2,5 +2,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://aosbnurzzoydxkpyygxu.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvc2JudXJ6em95ZHhrcHl5Z3h1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM2Mjk1ODIsImV4cCI6MjA2OTIwNTU4Mn0.1O5ie3nqTLrEb18lyLwFAF0ZHRyaNrO9bd1W47f6rVA"
 export const supabase = createClient(supabaseUrl!, supabaseKey!)
